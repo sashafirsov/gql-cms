ff545b532dd6dc3ef3edc6b21f1296b16539c273
Sasha Firsov <suns@firsov.net>
2025-10-19 16:43:51 -0700

    `admin-ui-e2e` lint fix

--- apps/admin-ui-e2e/eslint.config.mjs
+++ apps/admin-ui-e2e/eslint.config.mjs
@@ -2,11 +2,14 @@ import playwright from 'eslint-plugin-playwright';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  playwright.configs['flat/recommended'],
-  ...baseConfig,
-  {
-    files: ['**/*.ts', '**/*.js'],
-    // Override or add rules here
-    rules: {},
-  },
+    playwright.configs['flat/recommended'],
+    ...baseConfig,
+    {
+        files: ['**/*.ts', '**/*.js'],
+        // Override or add rules here
+        rules: {},
+    },
+    {
+        ignores: ['**/out-tsc', '**/next-env.d.ts'],
+    },
 ];
